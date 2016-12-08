1e0c75394cf308ec3e3e3ee19121d37a1c997a2a
Michael Tejada <michael@axiomcoding.com>
2016-12-07 20:55:16 -0700

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Array Methods
+Apply various Ruby array methods to pass each RSpec test. 
+
+##### Note: Each array is defined in the RSpec file meaning you do not need to define any arrays in the `array_methods.rb` file.
+##### Please see the [Ruby Array Documentation](https://ruby-doc.org/core-2.2.0/Array.html) for reference.
---
+++ array_methods.rb
@@ -0,0 +1,56 @@
+# define a method, sort_words, than takes in the array defined in the spec file, words
+# use a method that will sort the array alphabetically
+
+
+
+
+
+# define a method, sort_nums, than takes in the array defined in the rspec file, nums
+# use a method that will sort the array numerically from smallest to largest
+
+
+
+
+
+# define a method, reverse_method that takes in an array of cities as an argument
+# return the array reversed
+
+
+
+
+
+# define a method, first_method, that takes in an array of cities as an argument
+# return the first item in the array
+
+
+
+
+
+# define a method, last_method, that takes in an array of cities as an argument
+# return the last item in the array
+
+
+
+
+
+# define a method, count_method, that takes in an array of cities as an argument
+# return the number of items in the array
+
+
+
+
+# define a method, to_array_method, that takes in a range of numbers as an argument
+# return the range of numbers as an array, rather than a range
+# hint: use a method to convert the range to an array
+
+
+
+
+
+# define a method, include_method, that takes in 2 arguments, teams and team
+# check to see if the team is included in the teams array
+# hint: the .include? method requires an argument
+
+
+
+
